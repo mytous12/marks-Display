a58e21e794b86f117c32b771e44a6ad03cfdc484
Shivam <shivampundir@rocketmail.com>
2020-04-29 00:55:40 +0530

    Maven problem resolved

--- src/main/java/com/example/marksDisplay/controller/MarksController.java
+++ src/main/java/com/example/marksDisplay/controller/MarksController.java
@@ -1,4 +1,14 @@
 package com.example.marksDisplay.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/marks")
 public class MarksController {
+    @GetMapping("/demo")
+    public String demo() {
+        return "I'm running";
+    }
 }
