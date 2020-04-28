097f06c65f923c6134d073599dee14e6b0b49bb5
Rishabh <rishabhmalhotra091@gmail.com>
2020-04-29 03:16:37 +0530

    final commit

--- src/main/java/com/example/marksDisplay/controller/MarksController.java
+++ src/main/java/com/example/marksDisplay/controller/MarksController.java
@@ -5,20 +5,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping
 public class MarksController {
-
 
     @Autowired
     private MarksService marksService;
 
+    @PostMapping(path = "/addData")
+    public boolean addData(@RequestBody String data){
+        try{
+            return marksService.addData(data);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @PostMapping(path = "/getData")
+    public String getData(@RequestBody String email){
+        return marksService.getData(email);
+    }
+
     @GetMapping("/demo")
-    @GetMapping("/")
     public String demo() {
         return "I'm running";
     }
 
-    @PostMapping("/getOTP")
+    @PostMapping(path = "/getOTP")
     public String getOTP(@RequestBody String emailJSON){
         try{
             return marksService.sendOTP(emailJSON);
